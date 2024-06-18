5d40b64e3d7219ca03aa8f29ab769cb742bd6e17
Antoine <antoine.charvin@etu.umontpellier.fr>
2024-06-18 07:38:24 +0200

    modif deroulement parties simulations

--- scripts.js
+++ scripts.js
@@ -24,23 +24,43 @@ function backToTop() {
 document.querySelectorAll('.accordion-toggle').forEach(item => {
     item.addEventListener('click', event => {
         const target = document.querySelector(item.getAttribute('data-target'));
-        if (target.style.maxHeight) {
+        if (target.classList.contains('open')) {
             target.style.maxHeight = null;
             target.classList.remove("open");
         } else {
+            document.querySelectorAll('.accordion-content').forEach(content => {
+                content.style.maxHeight = null;
+                content.classList.remove("open");
+            });
             target.style.maxHeight = target.scrollHeight + "px";
             target.classList.add("open");
         }
     });
 });
 
+// Fonction pour vérifier si un élément est dans la vue
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
-document.addEventListener('DOMContentLoaded', function () {
-    const accordions = document.querySelectorAll('.accordion-toggle');
-    accordions.forEach((accordion) => {
-        accordion.addEventListener('click', function () {
-            const content = document.querySelector(this.dataset.target);
-            content.classList.toggle('open');
-        });
+// Ajoute la classe 'visible' aux éléments visibles
+function addVisibleClass() {
+    const sections = document.querySelectorAll('.section-animated');
+    sections.forEach(section => {
+        if (isInViewport(section)) {
+            section.classList.add('visible');
+        }
     });
-});
+}
+
+// Écouteur de défilement pour ajouter des animations aux sections
+window.addEventListener('scroll', addVisibleClass);
+
+// Ajoutez également cette fonction à l'événement de chargement de la page pour activer les animations sur les sections visibles dès le début
+window.addEventListener('load', addVisibleClass);
